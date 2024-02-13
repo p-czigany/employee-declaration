71b49f256479cf007ad097fb37350dcc6101268d
Peter Czigany <p.czigany@gmail.com>
2024-02-13 20:16:27 +0100

    update EmployeeDeclaration

--- src/main/java/org/peterczigany/employeedeclaration/EmployeeDeclaration.java
+++ src/main/java/org/peterczigany/employeedeclaration/EmployeeDeclaration.java
@@ -1,8 +1,60 @@
 package org.peterczigany.employeedeclaration;
 
-import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Entity
 public class EmployeeDeclaration {
-    private List<EmployeeStatement> employeeStatements;
+  @Id @GeneratedValue private Long id;
 
+  @OneToMany private Set<StatementAnswer> statementAnswers;
+  @ManyToOne private Employee employee;
+
+  public EmployeeDeclaration() {}
+
+  public EmployeeDeclaration(
+      Map<DeclarationStatement, Boolean> declarationAnswers, Employee employee) {
+    this(
+        null,
+        declarationAnswers.keySet().stream()
+            .map(statement -> new StatementAnswer(statement, declarationAnswers.get(statement)))
+            .collect(Collectors.toSet()),
+        employee);
+  }
+
+  public EmployeeDeclaration(Long id, Set<StatementAnswer> statementAnswers, Employee employee) {
+    this.id = id;
+    this.statementAnswers = statementAnswers;
+    this.employee = employee;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Set<StatementAnswer> getStatementAnswers() {
+    return statementAnswers;
+  }
+
+  public void setStatementAnswers(Set<StatementAnswer> statementAnswers) {
+    this.statementAnswers = statementAnswers;
+  }
+
+  public Employee getEmployee() {
+    return employee;
+  }
+
+  public void setEmployee(Employee employee) {
+    this.employee = employee;
+  }
 }
