daad4d77a98485dc47ccc5217e6f5eafb3baec28
Peter Czigany <p.czigany@gmail.com>
2024-02-17 11:19:40 +0100

    localize EmployeeDeclaration

--- src/main/java/org/peterczigany/employeedeclaration/EmployeeDeclaration.java
+++ src/main/java/org/peterczigany/employeedeclaration/EmployeeDeclaration.java
@@ -1,21 +1,27 @@
 package org.peterczigany.employeedeclaration;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Entity
+@Table(name = "nyilatkozattetel")
 public class EmployeeDeclaration {
   @Id @GeneratedValue private Long id;
 
   @OneToMany private Set<StatementAnswer> statementAnswers = new HashSet<>();
-  @ManyToOne private Employee employee;
+
+  @Column(name = "munkavallalo", nullable = false)
+  @ManyToOne
+  private Employee employee;
 
   public EmployeeDeclaration() {}
 
